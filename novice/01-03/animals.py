6cc9fa11c26000317e89b6f9c7f1cebd59af432a
anisarizqi <anisarizqi4@gmail.com>
2019-08-21 16:53:34 +0700

    function and module

---
+++ novice/01-03/animals.py
@@ -0,0 +1,7 @@
+def my_function(animals):
+	for x in animals:
+		print(x)
+
+animal = ["cat","dolphin","elephant"]
+
+my_function(animal)
---
+++ novice/01-03/city.py
@@ -0,0 +1,6 @@
+def my_function(fcity):
+	print(fcity)
+
+my_function("Bantul")
+my_function("ngemplak")
+my_function("tempel")
